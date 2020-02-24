81772d164d83714cee9920378c7f2993ace7c3ee
Andrei Dudnik <andreidudnik6@gmail.com>
2020-02-25 00:16:36 +0200

    Fix review problems

--- imports/ui/containers/ProductView.js
+++ imports/ui/containers/ProductView.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {withTracker} from "meteor/react-meteor-data";
 import Product from "../../models/Product";
 import ProductCard from "./../components/ProductCard";
 
 class ProductView extends Component {
-    render() {
-        return (
-            <div>
-              {/*не понял зачем здесь link*/}
-              {this.props.product && <ProductCard product={this.props.product} link={false}/>}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.props.product && <ProductCard product={this.props.product} link={false}/>}
+      </div>
+    );
+  }
 }
 
 export default withTracker((props) => {
   return {
-      // кавычки тут не нужны
-    product: Product.findOne({"_id": props.match.params.id})
+    product: Product.findOne({_id: props.match.params.id})
   }
 })(ProductView);
